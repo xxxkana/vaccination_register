41777984b4121eae36df7e360256f8e343025515
xxxkana <kana_m.0421@icloud.com>
2021-08-27 00:01:18 +0900

    個人番号登録機能実装3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#show"
   resource :home, only: :show
-  resources :users, only: [:index, :show, :edit, :update] do
+  resources :users, only: [:index, :show, :edit, :update, :new, :create] do
     resources :registrations, only: [:new, :create]
   end  
 end
